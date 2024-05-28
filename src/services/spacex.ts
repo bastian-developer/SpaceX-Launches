bce0f886be428196b385236520200ccc33c9c078
Satan <bastian.development@gmail.com>
2024-05-27 21:04:54 -0400

    Fixed single quote mark and increased launch quantity

--- src/services/spacex.ts
+++ src/services/spacex.ts
@@ -1,7 +1,14 @@
-import { type APISpaceXResponse } from "../types/api";
+import { type Doc, type APISpaceXResponse } from "../types/api";
+
+export const getLaunchById = async ({id} : {id : string}) => {
+  const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`)
+
+  const launch = (await res.json()) as Doc
+  return launch
+};
 
 export const getLatestLaunches = async () => {
-  const res = await fetch("https://api.spacexdata.com/v5/launches/query", {
+  const res = await fetch(`https://api.spacexdata.com/v5/launches/query`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -12,7 +19,7 @@ export const getLatestLaunches = async () => {
         sort: {
           date_unix: "asc",
         },
-        limit: 12,
+        limit: 30,
       },
     }),
   });
